4b116d94c174fde9ee66dcb76cea65496c0ee349
tshemsedinov <timur.shemsedinov@gmail.com>
2019-03-03 23:28:20 +0200

    Examples for yield and return

---
+++ JavaScript/b-yield.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function* counter(begin, end, delta) {
+  let value = begin;
+  while (end > value) {
+    value += delta;
+    yield value;
+  }
+}
+
+const c = counter(0, 30, 12);
+const val1 = c.next();
+const val2 = c.next();
+const val3 = c.next();
+const val4 = c.next();
+console.log({ c, val1, val2, val3, val4 });
