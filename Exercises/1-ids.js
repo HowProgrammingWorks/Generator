71060711afcf921e83b82a756809081e23ebafb5
kraftwerk28 <kefirchik3@gmail.com>
2020-01-12 14:22:05 +0200

    chore: refactor

--- Exercises/1-ids.js
+++ Exercises/1-ids.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const ids = function* (i = 1) {
+const ids = function* (i = 0) {
   for (;;++i) {
-    const left = 4 ** i;
-    for (let j = 0; j < 2 ** i; j++) {
+    const left = 4 << i * 2;
+    for (let j = 0; j < 2 << i; j++) {
       let res = 0;
-      for (let k = 0; k < i; k++) {
+      for (let k = 0; k < i + 1; k++) {
         res += (j & (1 << k)) << (k + 1);
       }
       yield (left + res).toString(2);
