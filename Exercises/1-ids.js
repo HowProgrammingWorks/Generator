a09e8891f1c355b407c6bd7c21c2c65fb50119c7
Vlad Dziuba <dzyubavlad@gmail.com>
2020-01-14 12:11:44 +0200

    Improve ids function
    Fixes: #5

--- Exercises/1-ids.js
+++ Exercises/1-ids.js
@@ -1,20 +1,27 @@
 'use strict';
 
-const ids = function* () {
-  const free = ['0'];
-  const prepared = { has: false, value: '' };
+const digits = ['00', '10', '1'];
+const LEADING_ONE = 2;
 
-  while (true) {
-    if (prepared.has) {
-      prepared.has = false;
-      yield prepared.value;
-    }
-    const nextFree = free.shift();
-    free.push('01' + nextFree);
-    free.push('00' + nextFree);
-    prepared.value = '11' + nextFree;
-    prepared.has = true;
-    yield '10' + nextFree;
+const inc = num => {
+  let i = num.length - 1;
+  let add = 1;
+  while (i > 0 && add === 1) {
+    const newAdd = num[i] & add;
+    num[i] ^= add;
+    add = newAdd;
+    i--;
+  }
+
+  if (i === 0 && add === 1) {
+    num.push(0);
+  }
+};
+
+const ids = function* () {
+  const num = [LEADING_ONE, 0];
+  for (;; inc(num)) {
+    yield num.map(d => digits[d]).join('');
   }
 };
 
